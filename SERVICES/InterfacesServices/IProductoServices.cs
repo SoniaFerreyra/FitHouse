3e60a04284fe859cf84f1b513442ddc7a34663c6
SoniaFerreyra <126492553+SoniaFerreyra@users.noreply.github.com>
2024-06-01 22:44:24 -0300

    Add files via upload

---
+++ SERVICES/InterfacesServices/IProductoServices.cs
@@ -0,0 +1,23 @@
+﻿using MODELS.Dtos;
+using MODELS.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SERVICES.InterfacesServices
+{
+    public interface IProductoServices
+    {
+        //Task Create(ProductoDtos productoDtos);
+
+        //Task Update(ProductoDtos productoDtos);
+
+        Task <IEnumerable<Producto>> Obtener();
+
+        //Task Delete(ProductoDtos  productoDtos);
+
+
+    }
+}
---
+++ SERVICES/ProductoServices.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using MODELS.Dtos;
+using MODELS.Entidades;
+using REPOSITORY.Repositorios;
+using SERVICES.InterfacesServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SERVICES
+{
+    public class ProductoServices : IProductoServices
+    {
+
+        private readonly IGeneryicRepository<Producto> _generyicRepo;
+        private readonly IMapper _mapper;
+
+        public ProductoServices(IGeneryicRepository<Producto> generyicRepository, IMapper mapper)
+        {
+            _generyicRepo = generyicRepository;
+            _mapper = mapper;
+
+        }
+
+       
+        public async Task<IEnumerable<Producto>> Obtener()
+        {
+            return await _generyicRepo.Obtener();
+        }
+    }
+}
